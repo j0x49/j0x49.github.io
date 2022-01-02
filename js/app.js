b0c13cfb3f5185406a54c7a68d83c5be268df773
The old J <96956257+j0x494949@users.noreply.github.com>
2022-01-02 21:57:26 +0300

    Kill

--- js/app.js
+++ js/app.js
@@ -1,7 +1,3 @@
-if (screen.width <= 699) {
-  document.location = "../mobile/"
-}
-
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const container = document.querySelector(".container");
